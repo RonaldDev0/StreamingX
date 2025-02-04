7353f79b723c770769a97a1165e2cecc53664478
Ronald <ronaldzamoradev@gmail.com>
2025-02-04 16:07:17 -0500

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,17 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/components/**/*.{ts,tsx}',
+    './src/app/**/*.{ts,tsx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        background: '#0a0a0a',
+        foreground: '#ededed'
+      }
+    }
   },
-  plugins: [],
-} satisfies Config;
+  plugins: []
+} satisfies Config
